8450b14774975bb138b49416551cc421fca1ac9c
Niyukat <niyacat@proton.me>
2026-07-05 20:52:33 +0200

    chore(): Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from './assets/logo.png'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src={logo} className="logo" alt="logo" />
+      </header>
+    </>
+  )
+}
+
+export default App
