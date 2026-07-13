79cc4491134fd301c76cc4d93adc004a099db06b
Niyukat <nakunaidev@proton.me>
2026-07-13 16:38:16 +0200

    style(task): Change Task.tsx to Task.ts

--- src/TaskCard.tsx
+++ src/TaskCard.tsx
@@ -1,15 +1,21 @@
+import type {ReactNode} from "react";
+
 type TaskCardProps = {
     onClick: () => void;
-    title: string
+    title: string;
+    actions: ReactNode
 };
 
-function TaskCard({title, onClick}: TaskCardProps) {
+function TaskCard({title, onClick, actions}: TaskCardProps) {
     return (
-        <>
-            <div onClick={onClick} className="task-card">
+        <div onClick={onClick} className="task-card" role="button">
+            <div className="task-card-header">
                 <h4 className="task-title">{title}</h4>
+                <div onClick={(event) => event.stopPropagation()} className="task-actions">
+                    {actions}
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 
