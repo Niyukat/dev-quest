8e1d8b1d901bc76fdb9c1866a8ede53561d80e9d
Niyukat <nakunaidev@proton.me>
2026-07-13 16:39:11 +0200

    feat(delete_task): Add delete task modal

---
+++ src/DeleteTaskModal.tsx
@@ -0,0 +1,25 @@
+import type {Task} from "./types/Task.ts";
+import Modal from "./Modal.tsx";
+
+type DeleteTaskModalProps = {
+    onConfirm: (task: Task) => void;
+    onClose: () => void;
+    task: Task;
+}
+
+function DeleteTaskModal({task, onConfirm, onClose}: DeleteTaskModalProps) {
+    function handleConfirm() {
+        onConfirm(task);
+        onClose();
+    }
+
+    return <Modal title={`Delete ${task.title}`} onClose={onClose}>
+        <p>Are you sure you want to delete this task ?</p>
+        <p className="delete-choices-container">
+            <button className="delete-confirm-btn" onClick={handleConfirm}>Yes</button>
+            <button className="delete-cancel-btn" onClick={onClose}>No</button>
+        </p>
+    </Modal>
+}
+
+export default DeleteTaskModal;
